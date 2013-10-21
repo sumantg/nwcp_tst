971f8c6af8cc8ba9620f377253d9350bcfe494ab
sumantg <sumantg@yahoo.com>
2013-10-21 11:03:36 +1100

    Updates_21.10.2013

--- personslist/personslist-web/src/main/webapp/personslist-web/personslist-web.controller.js
+++ personslist/personslist-web/src/main/webapp/personslist-web/personslist-web.controller.js
@@ -5,6 +5,10 @@ sap.ui.controller("personslist-web.personslist-web", {
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 */
+	
+	
+/* 
+=====>JSON Model START<====	
    onInit: function() 
    {
 	   var oData = {Persons:[{FirstName:"",LastName:""}
@@ -28,6 +32,34 @@ sap.ui.controller("personslist-web.personslist-web", {
 	   //oTable.unbindRows().bindRows("/Persons");
 	   oTable.bindRows("/Persons");
    }
+
+=====>JSON Model END<========
+*/
+	onInit : function() 
+	{
+		var sOrigin = window.location.protocol + "//" + window.location.hostname
+		+ (window.location.port ? ":" + window.location.port : "");
+		var personsListOdataServiceUrl = sOrigin + "/personslist-web/personslist.svc";
+		var odataModel = new sap.ui.model.odata.ODataModel(
+		personsListOdataServiceUrl);
+		this.getView().setModel(odataModel);
+	},
+	addPerson : function(sFirstName, sLastName, oTable) 
+	{
+		var persons = {};
+		persons.FirstName = sFirstName;
+		persons.LastName = sLastName;
+		this.getView().getModel().create("/Persons", persons, null, this.successMsg, this.errorMsg);
+		
+	},
+	successMsg : function() 
+	{
+	  sap.ui.commons.MessageBox.alert("Person entity has been successfully created");
+	},
+	errorMsg : function() 
+	{
+	  sap.ui.commons.MessageBox.alert("Error occured when creating person entity");
+	}
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 * (NOT before the first rendering! onInit() is used for that one!).
